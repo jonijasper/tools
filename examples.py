c97df63a396ea604386d2c10e7c7e4dd9a547ce4
jonijasper <jasper.alakotila@protonmail.com>
2025-07-03 01:24:22 +0300

    sheetwriter example

--- examples.py
+++ examples.py
@@ -2,10 +2,10 @@
 
 # examples.py
 """
-Example use of argparser module
+Example use of argparser and spread module
 
 Usage:
-    python3 examples.py -f "foo" -b "bar"
+    python3 examples.py -f foobar -b 1 2 3
 
 Demo:
     python3 examples.py --demo
@@ -13,10 +13,22 @@ Demo:
 Show this message:
     python3 examples.py --help
 """
+import pandas as pd 
+
 from parsers.argparser import argparser
+from helpers import sheetwriters
+
 
 def _demo():
     print(f"\N{goat}")
 
+def spreadsheet_example(file: str, values: list):
+    df = pd.DataFrame({ "foo": ["a", "b", "c"], 
+                        "bar": values, 
+                        "sheet": ["Sheet1", "Sheet1", "Sheet2"]})
+                        
+    sheetwriters.spread(df, f"{file}.xlsx", key="sheet", engine="xlsxwriter")
+
 if __name__ == "__main__":
-    argparser(help=__doc__, demo=_demo)
+    args = argparser(help=__doc__, demo=_demo)
+    spreadsheet_example(*args)
